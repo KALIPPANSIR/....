1354c14c1f4707f3d8497c33d8b429a0a446f97b
KALIPPANSIR <102481012+KALIPPANSIR@users.noreply.github.com>
2022-03-28 00:12:10 +0530

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3')
+  conn.sendFile(m.chat, API('mel', '/asupan', {}, 'apikey'), 'asupan.mp4', '© ᴀᴘᴀʀɴᴀ ᴍᴡᴏʟ ᴠ2')
 }
 handler.help = ['asupan']
 handler.tags = ['fun']
