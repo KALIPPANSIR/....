2643643d4bc6c06e50b080aebfb9a251427578b1
KALIPPANSIR <102481012+KALIPPANSIR@users.noreply.github.com>
2022-03-28 09:39:00 +0530

    Update settings.js

--- plugins/settings.js
+++ plugins/settings.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn }) => {
     let haruno = `┌ 「 𝐎𝐏𝐄𝐍 / 𝐂𝐋𝐎𝐒𝐄」
     ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴs ᴛᴏ ᴏᴘᴇɴ ᴏʀ ᴄʟᴏsᴇ ᴛʜᴇ ɢʀᴏᴜᴘ🙂
 `.trim()
-    await conn.send2ButtonLoc(m.chat, await (await fetch("https://telegra.ph/file/aeffa4c9d7b9f8f3a265b.jpg")).buffer(), haruno, '© ᴀᴊᴍᴀʟ-ᴀᴄʜᴜ', 'ᴏᴘᴇɴ', '.grup open', 'ᴄʟᴏsᴇ', '.grup close', m)
+    await conn.send2ButtonLoc(m.chat, await (await fetch("https://www.linkpicture.com/q/IMG-20220327-WA0949.jpg")).buffer(), haruno, '© ᴋᴀʟɪᴘᴘᴀɴsᴇʀ', 'ᴏᴘᴇɴ', '.grup open', 'ᴄʟᴏsᴇ', '.grup close', m)
 }
 handler.tags = ['grup']
 handler.help = ['open/close']
