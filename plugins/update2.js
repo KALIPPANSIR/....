eb26e94a5ae8c3b5a3062c1f1c54c48999517277
KALIPPANSIR <102481012+KALIPPANSIR@users.noreply.github.com>
2022-03-28 10:28:41 +0530

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'Ajmal-Achu/Nandhutty-V3'
+let repository = 'KALIPPANSIR/APARNA-MWOL-V2'
 let branch = 'main'
 
 async function handler(m, { text }) {
