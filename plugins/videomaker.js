31709ea808682e789fe43378073b38e4b0c812a2
KALIPPANSIR <102481012+KALIPPANSIR@users.noreply.github.com>
2022-03-28 10:30:06 +0530

    Update videomaker.js

--- plugins/videomaker.js
+++ plugins/videomaker.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   if (!mime) throw `Reply image with command *${usedPrefix + command}*`
   let media = await q.download()
   let url = await uploadImage(media)
-  await conn.sendFile(m.chat, API('xteam', '/videomaker/' + command, { url }, 'APIKEY'), 'shaunthesheep.mp4', 'ɴᴀɴᴅʜᴜᴛᴛʏ ᴠ3', m)
+  await conn.sendFile(m.chat, API('xteam', '/videomaker/' + command, { url }, 'APIKEY'), 'shaunthesheep.mp4', 'ᴀᴘᴀʀɴᴀ ᴍᴡᴏʟ ᴠ2', m)
 }
 handler.help = ['shaunthesheep', 'poly', 'glowing', 'colorful', 'army', 'retro']
 handler.tags = ['videomaker']
